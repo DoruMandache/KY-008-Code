1ccfbb51687688d027485459e21745c7dfe6434d
DoruMandache <56799921+DoruMandache@users.noreply.github.com>
2019-10-23 13:07:52 +0300

    Update Fade KY-008.ino

--- Fade KY-008.ino
+++ Fade KY-008.ino
@@ -1,4 +1,21 @@
-const int Pin_Laser = 9; 
+/* Sketch testare KY-008 cu Arduino IDE 1.8.3
+   by - Mandache Doru
+   
+   Materiale necesare testarii:
+     - placa de dezvoltare Arduino : UNO, Mega2560, NANO, Pro Mini
+     - KY-008 
+     - cabluri de lagatura
+     
+   Conexiuni:
+     Pini KY-008          Pini Arduino
+       S  ------------------ 10 (sau orice pin digital PWM 9, 10, 11, 3, 5, 6)
+       NC ------------------ neconectat
+       -  ------------------ GND
+       
+   Detalii modul KY-008: https://htecdb.blogspot.com/2019/10/ky-008.html    
+*/
+
+const int Pin_Laser = 10; 
  
 void setup() {
     pinMode (Pin_Laser, OUTPUT);
